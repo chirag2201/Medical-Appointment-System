100319da6c3df43c98fac24fc8d30f5ac9649459
chirag2201 <chiragawasthi580@gmail.com>
2025-02-22 14:21:24 +0530

    Intial update

--- appointments/admin.py
+++ appointments/admin.py
@@ -4,15 +4,14 @@ from appointments.models import Doctor, Patient, Appointment, GenericUser
 
 @admin.register(GenericUser)
 class GenericUserAdmin(admin.ModelAdmin):
-    list_display = ['id','username','phone_number','email']
+    list_display = ['id','user_name','phone_number','email']
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ['username','specialization','available_from','available_to']
-    # search_fields = ['user__username','specialization']
+    list_display = ['doctor_username','specialization','available_from','available_to']
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ['username','phone']
+    list_display = ['patient_username','phone_number']
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = ['patient_user','doctor_user','date','time', 'status']
--- appointments/models.py
+++ appointments/models.py
@@ -8,15 +8,15 @@ from django.db import models
 # user.save()
 
 class GenericUser(models.Model):
-    username= models.CharField(max_length=50)
+    user_name= models.CharField(max_length=50)
     phone_number =  models.CharField( max_length=50)
     email = models.CharField(max_length=100)
     
     def __str__(self) -> str:
-        return self.username
+        return self.user_name
 
 class Doctor(models.Model):
-    username = models.ForeignKey(GenericUser, on_delete=models.CASCADE)
+    doctor_username = models.ForeignKey(GenericUser, on_delete=models.CASCADE)
     specialization = models.CharField(max_length=100)
     available_from = models.TimeField()
     available_to = models.TimeField()
@@ -25,11 +25,11 @@ class Doctor(models.Model):
         return self.specialization
     
 class Patient(models.Model): 
-    username =  models.ForeignKey(GenericUser, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=15)
+    patient_username =  models.ForeignKey(GenericUser, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
 
     def __str__(self) -> str:
-        return self.phone
+        return self.phone_number
     
 class Appointment(models.Model):
     patient_user = models.ForeignKey(Patient, on_delete=models.CASCADE)
@@ -39,7 +39,7 @@ class Appointment(models.Model):
     status = models.CharField(max_length=20,choices=[('Pending','Pending'),('Confirmed','Confirmed'),('Cancelled','Cancelled')],default='Pending')
 
     def __str__(self) -> str:
-        return f"{self.patient_user.username} - {self.doctor_user.username}  {self.date}"
+        return f"{self.patient_user.patient_username.user_name} - {self.doctor_user.doctor_username.user_name}  {self.date} {self.time} {self.status}"
         
     
     
